933f6108e3f67d54799a945b061312435893ed54
Damjan Vincic <damjanvincic2003@gmail.com>
2021-12-01 15:57:40 +0100

    Dodat login.js

--- hzs_frontend/src/login/login.js
+++ hzs_frontend/src/login/login.js
@@ -1,26 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 
 import './login_stranica.css';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [submitted, setSubmitted] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleUsername = (e) => {
+        setUsername(e.target.value);
+        setSubmitted(false);
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        setSubmitted(false);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(username === '' || password === '')
+          setError(true);
+        else {
+          setError(false);
+          setSubmitted(true);
+        }
+    }
+
+    const successMessage = () => {
+      return (<div className="success" style={{display: submitted ? '' : 'none',}}>
+        <h4>Uspesno ste se ulogovali</h4>
+      </div>
+      );
+    }
+
+    const errorMessage = () => {
+      return(<div className="error" style={{display: error ? '' : 'none',}}>
+        <h4>Niste uneli sve podatke</h4>
+      </div>
+      );
+    }
+
     return(
-        <React.Fragment>
-        <div className="loginbox">
-            <img src="avataricon.png" class="avatar"/>
+        <>
+          <div class="loginbox">
+            <img src="slike/avataricon.png" class="avatar"/>
              <h1>Ulogujte se</h1>
              <form>
                <p>Korisnicko ime</p>
-               <input type="text" name="" placeholder="Unesite korisnicko ime"/>
+               <input onChange={handleUsername} type="text" value={username} placeholder="Unesite korisnicko ime"/>
                <p>Sifra</p>
-               <input type="password" name="" placeholder="Unesite sifru/"></input>
-               <input type="submit" name="" value="Ulogujte se"/>
-               <a href='#'>Izgubili ste lozinku?</a>
+               <input onChange={handlePassword} type="password" value={password} name="" placeholder="Unesite sifru"/>
+               <input onClick={handleSubmit} type="submit" name="" value="Ulogujte se"/>
+               <a href='#'>Izgubili ste lozinku?</a><br/>
                <a href='#'>Nemate nalog?</a>
              </form>
-
+              <div className="message" style={{background: submitted ? 'green':'red'}}>
+                {successMessage()}
+                {errorMessage()}
+              </div>
           </div>
-        </React.Fragment>
-    )
+        </>
+    );
 }
 
 export default Login;
