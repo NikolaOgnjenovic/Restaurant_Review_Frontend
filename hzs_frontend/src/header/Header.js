4d6a24ef9500e0f6edb899c466135ed84dce49d3
Damjan Vincic <damjanvincic2003@gmail.com>
2021-12-01 16:23:08 +0100

    Prebacen Header u React

--- hzs_frontend/src/header/Header.js
+++ hzs_frontend/src/header/Header.js
@@ -1,19 +1,24 @@
 import './Header.css'
+import logo from './logo1.jpg';
 
 const Header = ()=>{
     return(
-        <div id="header">
-            <div id="container">
-                <h2 id="title">Food nearby</h2>
-            </div>
+        <>
+        <header>
+            <img src={logo} class="logo"/>
+            <h1>Hrana blizu vas</h1>
             <nav>
-                <div><a href="/profil">Profil</a></div> 
-                <div><a href="/profil">Mapa</a></div>
-                <div><a href="/lista">Top lista</a></div>
-                <div><a href="/about">O nama</a></div>
+                <ul>
+                    <li><a href='#' class="active">Profil</a></li>
+                    <li><a href='#'>About</a></li>
+                    <li><a href='#'>Grafikon</a></li>
+                    <li><a href='#'>Lista</a></li>
+                    <li><a href='#'>Ulogujte se</a></li>
+                </ul>
             </nav>
-        </div>
-    )
+        </header>
+        </>
+    );
 }
 
 export default Header;
