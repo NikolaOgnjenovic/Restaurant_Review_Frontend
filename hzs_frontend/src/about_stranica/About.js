1ce1200685f3c8765ae7801f84b5c02a48e30356
Jorgenmungundar <95034520+Jorgenmungundar@users.noreply.github.com>
2021-12-01 23:01:46 +0100

    Add files via upload

---
+++ hzs_frontend/src/about_stranica/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <React.Fragment>
+        <div>
+            <div>
+            <h1>Mape:</h1>
+            <p>Hrana blizu vas je web aplikacija koja sluzi da pomogne ljudima u nepoznatom gradu da izaberu mesto gde bi mogli da jedu, popiju kafu, kupe pecivo...<hr/>
+            Ova web aplikacija sadrzi pristup google mapama kako bi ste mogli brzo da pronadjete mesto za jelo koje vam je blizu, ali posto ne znate da li je hrana dobra<hr/>
+            u mestu koje ste izabrali mozete pogledati recenzije svakog ugostiteljskog objekta na mapi.<hr/>Na recenzijama takodje mozete videti preporuke obroka koje su nasi korisnici 
+            vec probali kao i njihovu cenu</p>
+            <h2>Recenzije:</h2>
+            <p>Ukoliko vam je neka od recenzija sa naseg sajta pomogla mozete podrzati korisnika lajkovanjem njegove recenzije, ili dislajkovanjem ukoliko ste se uverili da je recenzija 
+                <hr/> neistinita. Takodje i vi mozete ostaviti svoju recenziju kako biste pomogli i drugim korisnicima da izaberu svoj danasnji obrok.</p>
+            <h3>Korisnici:</h3>
+            <p>Da biste mogli u potpunosti da koristite "Hrana blizu vas" trebalo bi da se registrujete na sajtu kako biste dobili mogucnost pisanja recenzija.<hr/>
+            Registracija traje veoma kratko i sve sto Vam je za nju potrebno je E-mail adresa. Nakon sto napravite nalog on ce biti dodat u top listu korisnika u kojoj napredujete<hr/>
+            pomocu lajkova na recenzijama odnosno spustate se dislajkovima.</p>
+            <h4>Uzivajte u dobroj hrani!!!</h4>
+        </div>
+       </div>
+       </React.Fragment>
+    )
+}
+
+export default About;
---
+++ hzs_frontend/src/mapping/MapPage.js
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import ReactDOM from 'react-dom';
+import Header from "../header/Header";
+import { MapsWindow } from "./Maps";
+import Reviews from "../components/Reviews";
+
+import {AiFillLike, AiOutlineLike} from "react-icons/ai";
+import {AiFillDislike} from "react-icons/ai";
+
+import './MapPage.css';
+import Review from "../components/Review";
+
+const MapPage = () =>{
+    const [review, setReview] = useState({
+        id: -1,
+        place_id: "",
+        title: "",
+        description: "",
+        likes: 0,
+        dislikes: 0,
+        foodCost: -1,
+        userId: -1,
+        likedBy: [],
+        dislikedBy: []
+    });
+
+    return(
+        <React.Fragment>
+            <div id="flex-container">
+                <div id="map_win_container">
+                    <MapsWindow onMarkerClick={CreateReview}></MapsWindow>
+                </div>
+                <div id="review_form"></div>
+            </div>
+            <div id="list">
+                <Reviews/>
+            </div>
+        </React.Fragment>
+    );
+}
+
+const CreateReview = (place, address) =>{
+    const serverURL = "https://hrana-u-blizini-api.herokuapp.com/reviews/getReviewsByPlaceId";
+
+    console.log("Place is : ", place);
+    console.log("Address : ",address);
+
+    const addressFormated = `${address[2].long_name}, ${address[1].long_name} ${address[0].long_name}`;
+
+    try{
+        fetch(`${serverURL}${place.place_id}`)
+            .then(({reviews})=>{
+                console.log(reviews);
+                document.getElementById("list").style.display = 'none';
+                {reviews && reviews.map((review)=>{
+                    <Review item={review.json()}/>
+                })};
+        });
+    }catch(e){
+        console.log(e);
+    }
+
+    const formElement = ()=>{
+        return (
+            <React.Fragment>
+                <div id="form_container">
+                <h3>{place[0].structured_formatting.main_text}</h3>
+                <h5>{addressFormated}</h5>
+                <div>
+                    <Form place={place}></Form>
+                </div>
+                </div>
+            </React.Fragment>
+            );
+    }
+    ReactDOM.render(formElement(), document.getElementById("review_form"));
+}
+
+const Form = ({place}) => {
+    return (
+        <div id="form_wrapper">
+            <form>
+                <div>
+                <label>Vaše korisničko ime</label><br/>
+                <input id="username" type="text" placeholder=""/><br/>
+                </div>
+                <div>
+                <label> Vaše misljenje: </label><br/>
+                <textarea
+                    id="description"
+                    rows="8"
+                    cols="50" 
+                    placeholder="Hrana je bila izvrsna!"
+                />
+                </div>
+                <div>
+                <label> Cena jela: </label><br/>
+                <input tyep="text" placeholder="Koliko vas je koštao obrok" id="foodCost"/>
+                </div>
+                <div>
+                    <input type="submit" value="Pošalji recenziju"
+                            onSubmit={()=>{
+                                onSubmit(place);
+                            }}/>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+const onSubmit=(place)=>{
+    const postURL = "https://hrana-u-blizini-api.herokuapp.com/reviews/";
+    const getUserEmailURL = `https://hrana-u-blizini-api.herokuapp.com/user/${document.getElementById("username").value()}/getUser`;
+    const review = {
+        description: document.getElementById("description").value(),
+        foodCost: document.getElementById("foodCost").value(),
+        placeId: place.place_id,
+        title: place[0].structured_formatting.main_text,
+        userEmail: fetch(getUserEmailURL).then((data)=>data.json()).then((user)=>user.email)
+    }
+    const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(review)
+    }
+
+    fetch(postURL, requestOptions)
+        .then((data)=>data.json())
+        .then((response)=>console.log(response));
+}
+
+export default MapPage;
